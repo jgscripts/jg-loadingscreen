3b12b528e1c804dc9015d9c5719ccefb348511d1
James Grimshaw <jamesrichardgrimshaw@gmail.com>
2022-07-30 04:14:01 +0100

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'The world\'s greatest loading screen'
+version '1.0'
+author 'JG Scripts'
+
+files {'*.html', 'assets/**/*.*', 'assets/**/**/*.*'}
+
+loadscreen 'index.html'
+
+lua54 'yes'
